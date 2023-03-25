e15535e14875d090d0e6092c7d2e7cd4e232c378
Danny Wilson <111dannywilson@gmail.com>
2023-03-25 00:52:31 -0700

    First commit

---
+++ password-generator.py
@@ -0,0 +1,65 @@
+import string
+import random
+
+alphabets = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+values = []
+
+print("Note, password length should be integers")
+print('-------------')
+
+
+# making user input an integer
+def check_input():
+    password_length = input("Password length: ")
+    try:
+        password_length = int(password_length)
+    except:
+        print('Password length must be an integer')
+        exit()
+    return password_length
+
+# def check_password_length(variable, limit):
+#     if len(variable) < limit:
+#         print(f'Lenght is too huge, limit is {len(variable)}')
+#         exit()
+#     else:
+#         return variable[0:limit]
+
+# function for getting a specific type of value
+def get_values(module, loop_variable):
+    for loop_variable in module:
+        values.append(loop_variable)
+
+
+# function for shuffling values
+def shuffle_values(limit):
+        shuffled_values = []
+        while len(shuffled_values) != len(values):
+            all_values = random.choice(values)
+            shuffled_values.append(all_values)
+
+        # checking for password length gainst all characters length(shuffled_values)
+        if len(shuffled_values) < limit:
+            print(f'Lenght is too huge, limit is {len(shuffled_values)}')
+            exit()
+        else:
+            return shuffled_values[0:limit]
+
+
+
+# appending all types of values to the values list..
+get_values(alphabets, 'alphabet')
+get_values(digits, 'digit')
+get_values(symbols, 'symbol')
+
+
+password_limit = check_input()
+shuffled_values = shuffle_values(password_limit)
+
+# stringifying shuffled_values(a list)
+password = ''.join(map(str, shuffled_values))
+print(password)
+ 
